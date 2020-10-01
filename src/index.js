c3f5ae2bc2a748ace6d941688865a01e7e077c20
slavaScript2020 <vv.koval.2019@gmail.com>
2020-10-01 23:18:19 +0300

    init: added max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array === undefined || array.length === 0){
+	return 0;
+	} 
+	let minValue = array[0];
+	for(let i = 0; i < array.length; i++){
+		if( array[i] < minValue ) {
+			minValue = array[i];
+		}
+	}
+    return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array === undefined || array.length === 0){
+	return 0;
+	} 
+	let maxValue = array[0];
+	for(let i = 0; i < array.length; i++){
+		if( array[i] > maxValue ) {
+			maxValue = array[i];
+		}
+	}
+    return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array === undefined || array.length === 0){
+	return 0;
+	} 
+	let avgValue = 0;
+	for(let i = 0; i < array.length; i++){ 
+		avgValue += array[i];
+	}
+    return avgValue / array.length ;
 }
